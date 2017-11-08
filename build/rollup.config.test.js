d427b69018505983432f68753cc875f7e81d7e31
skylerlee <skyler.ac.lee@gmail.com>
2017-11-08 14:47:09 +0800

    Update testing build config

--- build/rollup.config.test.js
+++ build/rollup.config.test.js
@@ -1,4 +1,20 @@
 'use strict'
 
 import baseConfig from './rollup.config.base'
-export default baseConfig
+import multiEntry from 'rollup-plugin-multi-entry'
+import coffee from 'rollup-plugin-coffee-script'
+import babel from 'rollup-plugin-babel'
+
+export default Object.assign(baseConfig, {
+  input: './test/**/*.spec.coffee',
+  output: {
+    file: './dist/specs.js',
+    name: 'specs',
+    format: 'umd'
+  },
+  plugins: [
+    multiEntry(),
+    coffee(),
+    babel()
+  ]
+})
