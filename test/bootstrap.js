aaa7de206e92767d20ecd71a23a77bcd1b570471
skylerlee <skyler.ac.lee@gmail.com>
2017-11-08 21:34:36 +0800

    Add specs register

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -3,3 +3,11 @@
  * Use of this source code is governed by the MIT license that can be
  * found in the LICENSE file.
  */
+
+function register (runner) {
+  runner.on('end', () => {
+    console.log(runner)
+  })
+}
+
+exports.register = register
