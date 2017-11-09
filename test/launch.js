7dd933c2b99f0ca49e5554e80facde879276725e
skylerlee <skyler.ac.lee@gmail.com>
2017-11-09 13:48:15 +0800

    Add test message handler

--- test/launch.js
+++ test/launch.js
@@ -7,6 +7,23 @@ const launcher = require('chrome-launcher')
 let server = new ws.Server({
   port: 9020
 })
+let handler = {
+  init (socket) {
+    console.log('Test runner start')
+    socket.on('message', msg => {
+      (this[msg.type] || this.noop).call(this, msg)
+    })
+  },
+  passed (msg) {
+    console.log('Test passed')
+  },
+  failed (msg) {
+    console.log('Test failed')
+  },
+  noop () {}
+}
+
+server.on('connection', socket => handler.init(socket))
 
 launcher.launch({
   chromeFlags: ['--allow-file-access-from-files'],
